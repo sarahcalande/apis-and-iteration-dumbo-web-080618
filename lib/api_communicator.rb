94048d66cea9a8a9c081209bff32d29a6dae2a89
sarahcalande <scalande@yahoo.com>
2018-08-07 13:31:04 -0400

    Done.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,12 +2,26 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+
 def get_character_movies_from_api(character)
   #make the web request
+
+  films_array = []
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
+
+
+  response_hash["results"].each do |results_hash|
+    results_hash["name"]
+
+    if character == results_hash["name"].downcase
+      films_array = results_hash["films"]
+    end
+
+  end
+
+  films_array
+  # NOTE: in this demonstration we name many of the variables _hash or _array.
   # This is done for educational purposes. This is not typically done in code.
 
 
@@ -22,14 +36,29 @@ def get_character_movies_from_api(character)
   #  of movies by title. play around with puts out other info about a given film.
 end
 
-def print_movies(films_hash)
+
+def print_movies(films_array)
+  if films_array.length > 0
+    for i in 0..films_array.length-1 do
+      response_string = RestClient.get(films_array[i])
+      response_hash = JSON.parse(response_string)
+      puts "#{i+1}. #{response_hash["title"]}"
+    end
+  else
+    puts "The person you input is not a character in the Starwars movie Universe"
+  end
+
   # some iteration magic and puts out the movies in a nice list
 end
 
+
 def show_character_movies(character)
+
   films_array = get_character_movies_from_api(character)
   print_movies(films_array)
 end
+
+
 
 ## BONUS
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
+
 def welcome
+  puts "Welcome to Felix and Sarahs amazing work of StarWars character movie finder thanks for coming!!!?"
   # puts out a welcome message here!
 end
 
 def get_character_from_user
   puts "please enter a character"
+  user_input = gets.strip.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
